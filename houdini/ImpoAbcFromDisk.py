55d134e7ad04121944da043e4aa5485583a141c1
Aaron <910854285@qq.com>
2019-09-02 16:52:32 +0800

    update 1.5.1 1. fixed some bugs 2. integrated into MainWindow 3. Added double click event 4. Update ui to v2 5. Added go previous pushbutton

--- houdini/ImpoAbcFromDisk.py
+++ houdini/ImpoAbcFromDisk.py
@@ -7,14 +7,16 @@ try:
     import hou
 except ImportError:
     pass
-sys.path.append('../ui')
 try:
-    from ui import import_alembic as ImAl
+    from ui import import_alembic_v02 as ImAl
+    from utils import MyUtils
 except Exception:
     # If have imported automation module
-    from automation.ui import import_alembic as ImAl
+    from automation.ui import import_alembic_v02 as ImAl
+    from automation.utils import MyUtils
 import ViewAlembicListModel
 import houdini_core as ho
+
 from PySide2 import QtWidgets, QtCore
 
 
@@ -26,19 +28,39 @@ class HoudiniWidget_AlemImport(QtWidgets.QWidget, ImAl.Ui_Widget_ImportAlembic):
             self.setParent(hou.qt.mainWindow(), QtCore.Qt.Window)
         except NameError:
             pass
-        self.data = None
         self.root_path = r""
+        # set Item View
         self.listWidget_alembiclist.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+        # set some styles
+
         self.set_connect()
 
     def set_connect(self):
-        self.btn_searchpath.clicked.connect(self.setListWidget)
-        self.lineEdit_alembicpath.textChanged.connect(self.setStatus)
         self.btn_alembic_single.clicked.connect(self.export_alembic_single)
         self.btn_alembic_archive.clicked.connect(self.export_alembic_archive)
+        self.btn_searchpath.clicked.connect(self.setListWidget)
+        self.btn_go_previous_level.clicked.connect(self.SwitchPreviousLevel)
+        self.lineEdit_alembicpath.textChanged.connect(self.setStatus)
+        self.listWidget_alembiclist.doubleClicked.connect(self.SwitchNextLevel)
+
+    # Set next level's view
+    def SwitchNextLevel(self):
+        temp_seleted = ''
+        for i in self.listWidget_alembiclist.selectedIndexes():
+            temp_seleted = i.data()
+        temp_path = self.root_path + temp_seleted
+        MyUtils.DebugInfo(temp_path, theMessageFrom="NextLevel Path", show=1)
+        if os.path.isdir(temp_path):
+            self.setListWidget(IncomingPath=temp_path)
+
+    # Set previous level's view
+    def SwitchPreviousLevel(self):
+        previous_path = MyUtils.get_previous_level_path(self.root_path)
+
+        self.listWidget_alembiclist.clear()
+        self.setListWidget(IncomingPath=previous_path)
 
     def setStatus(self):
-        self.data = None
         self.listWidget_alembiclist.clear()
 
     def export_alembic_single(self):
@@ -78,28 +100,43 @@ class HoudiniWidget_AlemImport(QtWidgets.QWidget, ImAl.Ui_Widget_ImportAlembic):
             if not str(files[i]).endswith('.abc'):
                 raise NameError("Select error")
 
-    def setListWidget(self):
-        if self.data == None:
-            path = self.lineEdit_alembicpath.text()
-            if not len(str(path)):
-                try:
-                    path = os.path.dirname(hou.hipFile.path())
-                except NameError:
-                    print( "NameError: Can not get hipFile path")
-                    path = os.path.dirname('.')
+    def setListWidget(self, IncomingPath=''):
+        # clean has been exists view list
+        self.listWidget_alembiclist.clear()
+        # The default set
+        path = self.lineEdit_alembicpath.text()
+        if os.path.exists(IncomingPath):
+            path = IncomingPath
+        if not len(str(path)):
+            try:
+                path = os.path.dirname(hou.hipFile.path())
+                MyUtils.DebugInfo(path, "Geted: Curent hip file path", show=1)
+            except NameError:
+                win_desktop_folder = MyUtils.get_windows_dekstopFolder()
+                path = os.path.dirname(win_desktop_folder)
+                MyUtils.DebugInfo("NameError: Not able to get the hipFile path, geted Desktop directory", show=1)
+        elif os.path.exists(path):
+            # Uniform backslashes
+            if os.path.sep in path:  # win: \  ==> Instead of /
+                path = str(path).replace(os.path.sep, '/')
+        else:
+            raise MyUtils.PathIrregularError("Irregular: %s " % (path, ))
+        # Uniform slashes by different System
+        path = os.path.normpath(path)
+        # fix the path
+        if not path.endswith(os.sep):
+            path += os.sep
 
-            else:
-                if not os.path.exists(path):
-                    raise NameError("The path Error")
+        # sync list weight's content with line edit's content same
+        self.lineEdit_alembicpath.setText(path)
+        reload(ViewAlembicListModel)
+        model = ViewAlembicListModel.Houdini_Import_Alembic_FromDisk(path)
+        ale_lists, dir_lists, ale_path_dir, dir_path = model.getList()
+        self.root_path = path
+        MyUtils.DebugInfo(self.root_path, theMessageFrom='From Sel List Weight  ==> root_path', show=0)
 
-            reload(ViewAlembicListModel)
-
-            model = ViewAlembicListModel.Houdini_Import_Alembic_FromDisk(path)
-            ale_lists, ale_path_dir, dir_path = model.getList()
-            self.root_path = dir_path
-
-            self.listWidget_alembiclist.addItems(ale_lists)
-            self.data = 1
+        self.listWidget_alembiclist.addItems(ale_lists)
+        self.listWidget_alembiclist.addItems(dir_lists)
 
     def keyPressEvent(self, e):
         if e.key() == QtCore.Qt.Key_Return:
