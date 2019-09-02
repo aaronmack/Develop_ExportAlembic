0afda3b9db7d31cc8b0bd8f909ce33fc9024c9dd
Aaron <910854285@qq.com>
2019-09-02 16:53:15 +0800

    update 1.5.1 1. added dir lists

--- houdini/ViewAlembicListModel.py
+++ houdini/ViewAlembicListModel.py
@@ -3,23 +3,29 @@
 
 import os
 
+
 class Houdini_Import_Alembic_FromDisk(object):
     def __init__(self, path):
-        super(Houdini_Import_Alembic_FromDisk, self).__init__()
         self.source_path = path
 
     def getList(self):
-        alembic_file_path = self.source_path
-        ale_list = os.listdir(alembic_file_path)
+        ale_list = os.listdir(self.source_path)
         ale_lists = []
+        dir_lists = []
         ale_path_dir = {}
         for x in ale_list:
-            j = alembic_file_path + os.sep + x
+            j = self.source_path + os.sep + x
             if os.path.isdir(j):
                 ale_path_dir[x] = j
-            else:
+                dir_lists.append(x)
+            elif os.path.isfile(j):
                 ale_lists.append(x)
-        dir_path = os.path.abspath(alembic_file_path)
+            else:
+                raise NameError('unknown error')
+        dir_path = os.path.abspath(self.source_path)
 
-        return ale_lists, ale_path_dir, dir_path
+        return ale_lists, dir_lists, ale_path_dir, dir_path
 
+
+if __name__ == '__main__':
+    pass
